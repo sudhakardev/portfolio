0ef77859e2ea1e0493ca1478c1473b4abcc4c33a
sudhakar <sudhakark4227@gmail.com>
2026-02-08 12:06:46 +0530

    Fix: Universal deployment support using HashRouter and relative paths

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import { motion, useScroll, useSpring } from "framer-motion";
@@ -26,12 +26,12 @@ const App = () => {
         />
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="/Sudhakar-Portfolio">
+        <HashRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </TooltipProvider>
     </QueryClientProvider>
   );
